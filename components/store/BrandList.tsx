799d8e29382717e02be235c75abd3459257f0228
Piyush Thakur <piyushthakur241199@gmail.com>
2025-07-13 01:02:11 +0530

    brand link

--- components/store/BrandList.tsx
+++ components/store/BrandList.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { Brand } from "@/types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -7,6 +10,12 @@ interface Props {
 }
 
 const BrandList = ({ brands }: Props) => {
+  const router = useRouter();
+
+  const handleClick = (path: string) => {
+    router.push(`/brand/${path}?page=1`);
+  };
+
   return (
     <div className="bg-white py-12 px-4 text-center">
       <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-8">
@@ -16,7 +25,8 @@ const BrandList = ({ brands }: Props) => {
         {brands?.map((item) => (
           <div
             key={item.id}
-            className="w-20 h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 flex items-center justify-center bg-gray-100 rounded-full overflow-hidden"
+            className="w-20 h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 flex items-center justify-center bg-gray-100 rounded-full overflow-hidden cursor-pointer"
+            onClick={() => handleClick(item?.slug)}
           >
             <Image
               src={item.cardImage || ""}
